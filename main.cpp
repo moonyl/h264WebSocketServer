4579c015695be8d3e971bb282a2ed53ca074e87e
moonyl <moonysl@hanmail.net>
2019-11-12 13:48:11 +0900

    데이터에 rendering 할 부분을 실어서 보내도록 함.

--- main.cpp
+++ main.cpp
@@ -12,6 +12,29 @@ void sendRawStreamOnSocketContext(QWebSocket& socket, const uint8_t* buf, int si
     QThread::msleep(30);
 }
 
+void sendRawStreamWithShowInfoOnSocketContext(QWebSocket& socket, const uint8_t* buf, int size, bool show = true)
+{
+    QByteArray packet("psd");
+    packet.append(show == true ? 1 : 0);
+//    for (int i = 3; i >= 0; i--) {
+//        packet.append((size >> i*8) & 0xff);
+//    }
+    for (int i = 0; i < 4; i++) {
+        packet.append((size >> i*8) & 0xff);
+    }
+    //packet.append(QByteArray::number(size));
+    QByteArray rawData((const char*)buf, size);
+    packet.append(rawData);
+    //QMetaObject::invokeMethod(&socket, [&socket, rawData](){
+    QMetaObject::invokeMethod(&socket, [&socket, packet](){
+        //socket.sendBinaryMessage(rawData);
+        socket.sendBinaryMessage(packet);
+        //std::cout << "size: " << rawData.size() << std::endl;
+        std::cout << "size: " << packet.size() << std::endl;
+    });
+    QThread::msleep(30);
+}
+
 #include <QMap>
 
 int main(int argc, char **argv)
@@ -25,7 +48,8 @@ int main(int argc, char **argv)
         dispatcherTable[&socket] = QSharedPointer<StreamDispatcher>::create("baseline.h264");
 
         auto onRead = [&socket](const uint8_t* buf, int size){
-            sendRawStreamOnSocketContext(socket, buf, size);
+            //sendRawStreamOnSocketContext(socket, buf, size);
+            sendRawStreamWithShowInfoOnSocketContext(socket, buf, size, true);
         };
         dispatcherTable[&socket]->start(onRead);
     }};
